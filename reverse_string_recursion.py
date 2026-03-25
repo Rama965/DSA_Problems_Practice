5e353777c18000bbd07576717eecf59073ec37a9
Rama <bandirama940@gmail.com>
2026-03-25 20:08:58 +0530

    dsa practice

--- reverse_string_recursion.py
+++ reverse_string_recursion.py
@@ -1,3 +1,12 @@
+#Reverse String Using Recursion
+#Input: hello
+#Output: olleh
+#reverse("hello")
+#= reverse("ello") + h
+#= reverse("llo") + e + h
+#= reverse("lo") + l + e + h
+#= reverse("o") + l + l + e + h
+#= o + l + l + e + h
 def reverse_string(s):
     if len(s) == 0:
         return s
