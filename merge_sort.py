3e6df8488321268f1ab604eada6acf8d94dc6dbb
Rama <bandirama940@gmail.com>
2026-03-29 11:37:10 +0530

    dsa practice

---
+++ merge_sort.py
@@ -0,0 +1,28 @@
+#Merge Sort (Divide & Conquer)
+#Merge Sort works like this:
+#Divide array into halves
+#Sort each half recursively
+#Merge both sorted halves
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge(left,right)
+
+def merge(left,right):
+    result = []
+    i=j=0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+print(merge_sort([5,3,8,4,2]))
