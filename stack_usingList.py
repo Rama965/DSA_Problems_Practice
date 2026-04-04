7a334fe157ab7c33fcae759a69dfaaffc49a5dca
Rama <bandirama940@gmail.com>
2026-04-04 19:39:22 +0530

    dsa practice

---
+++ stack_usingList.py
@@ -0,0 +1,17 @@
+stack = []
+
+#push
+stack.append(10)
+stack.append(20)
+stack.append(30)
+print(stack)
+
+#pop
+stack.pop()
+print(stack)
+
+#peek
+print(stack[-1])
+
+#is empty
+print(len(stack)==0)
